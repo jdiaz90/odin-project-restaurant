9164bf8d9c92a9f1e44ac50add87d438fdbd51ae
Javier Díaz <jdr_1_2_1990@hotmail.com>
2023-03-06 23:04:34 +0100

    Fill div#content

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('Hello World!')
+import h1 from './h1'
+import description from './divDescription'
+import info from './info'
+
+const body = document.querySelector('#content')
+body.append(h1())
+body.append(description())
+body.append(info())
