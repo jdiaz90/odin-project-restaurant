1b66ac7f8de5ca2db3b9f8245b2e9a9fd9afa87b
Javier Díaz <jdr_1_2_1990@hotmail.com>
2023-03-06 23:53:57 +0100

    Refactor src folder

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-import h1 from './h1'
-import description from './divDescription'
-import info from './info'
+import './css/style.css';
+
+import h1 from './js/h1'
+import description from './js/divDescription'
+import info from './js/info'
 
 const body = document.querySelector('#content')
 body.append(h1())
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: './src/index.js',
@@ -6,4 +7,17 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+       }
+    ],
+  },
+  plugins: [new MiniCssExtractPlugin()],
 };
